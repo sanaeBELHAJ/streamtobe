4231178fdc9c49c7ce14cbd731cec4944bc7490a
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-03-22 22:17:19 +0100

    Structure de la table Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('firstname');
+            $table->string('pseudo')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('status')->default(0);
+            $table->text('description');
             $table->rememberToken();
             $table->timestamps();
         });
