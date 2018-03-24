32ede76911964fa4d54172b39d7b7b5f5ddd4268
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-03-24 19:55:00 +0100

    Ajout d'un contrôle sur la confirmation du compte avant authentification

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,4 @@ Route::get('/user/verify/{confirmation_code}', ['as' => 'verify', 'uses' => 'Use
     });
 */
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
