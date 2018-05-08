6c487023cf0529d49237c2be3aa9089104f8434a
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-05-08 10:32:08 +0200

    Réorganisation de l'affichage de la liste des streams

--- resources/views/stream/index.blade.php
+++ resources/views/stream/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-		<div class="col-sm-offset-4 col-sm-4">
+		<div class="col-12">
 			@if(session()->has('ok'))
 				<div class="alert alert-success alert-dismissible">{!! session('ok') !!}</div>
 			@endif
@@ -10,33 +10,41 @@
 				<div class="panel-heading">
 					<h3 class="panel-title">Liste des streams</h3>
 				</div>
-				<table class="table">
+				<table class="table col-12">
 					<thead>
 						<tr>
-							<th>Pseudo</th>
 							<th>Image</th>
+							<th>Pseudo</th>						
 							<th>Statut</th>
 						</tr>
 					</thead>
 					<tbody>
-						@foreach ($streams as $stream)
+						@if(count($streams) > 0)
+							@foreach ($streams as $stream)
+								<tr>
+									<td>
+										<img class="col-7 col-sm-5 col-md-2" src="<?php echo asset('storage/'.$stream->user->avatar); ?>" 
+											alt="" title="Image de profil">
+									</td>
+									<td class="text-primary">
+										<strong>{!! $stream->user->pseudo !!}</strong>
+									</td>
+									<td>
+										@if($stream->status==1)
+											{!! link_to_route('stream.show', 'En ligne', [$stream->user->pseudo], ['class' => 'btn btn-success btn-block']) !!}
+										@else
+											{!! link_to_route('stream.show', 'Hors ligne', [$stream->user->pseudo], ['class' => 'btn btn-secondary btn-block']) !!}
+										@endif
+									</td>
+								</tr>
+							@endforeach
+						@else
 							<tr>
-								<td class="text-primary">
-									<strong>{!! $stream->user->pseudo !!}</strong>
-								</td>
-								<td>
-									<img class="pictureAccount" src="<?php echo asset('storage/'.$stream->user->avatar); ?>" 
-										alt="" title="Image de profil">
-								</td>
-								<td>
-									@if($stream->status==1)
-										{!! link_to_route('stream.show', 'En ligne', [$stream->user->pseudo], ['class' => 'btn btn-success btn-block']) !!}
-									@else
-										{!! link_to_route('stream.show', 'Hors ligne', [$stream->user->pseudo], ['class' => 'btn btn-secondary btn-block']) !!}
-									@endif
+								<td colspan="3" class="text-center">
+									<i>Vous ne suivez actuellement aucun stream.</i>
 								</td>
 							</tr>
-						@endforeach
+						@endif
 					</tbody>
 				</table>
 			</div>
@@ -46,8 +54,12 @@
 
 @section('css')
 <style>
+	.table td{
+		vertical-align: middle;
+	}
+
 	.pictureAccount{
-		width: 30%;
+		width: 10%;
 	}
 </style>
 @endsection
