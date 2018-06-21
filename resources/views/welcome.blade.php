b35058b83b06a138af7eda9ed3ee1af20fe39893
sanae <sanai-son112@hotmail.fr>
2018-06-22 01:43:28 +0200

    #2 text backgroud fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,25 +10,31 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active" style="background-image: url('img/kitchen-ready-for-cooking_4460x4460.jpg')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Vous aimez la cuisine!</h3>
-              <p>Venez montrer vos talents chef sur votre chaine.</p>
+            <div class="carousel-caption d-none d-md-block ">
+                <div class="visible">
+                     <h3>Vous aimez la cuisine!</h3>
+                     <p>Venez montrer vos talents chef sur votre chaine.</p>
+                </div>
               <a href="{{ route('stream.index') }}"  class="btn btn-lg gold">Regarder nos chaines </a>
               <a href="{{ route('register') }}"  class="btn  btn-lg gold">Créer votre chaine</a>
             </div>
           </div>
           <div class="carousel-item" style="background-image: url('img/woman-playing-guitar_4460x4460.jpg')">
             <div class="carousel-caption d-none d-md-block">
-              <h3>Vous aimez chanter? Vous savez jouer sur un instrument musical!</h3>
-              <p>Vous pouvez vous montrer en public!</p>
+              <div class="visible">
+                <h3>Vous aimez chanter? Vous savez jouer sur un instrument musical!</h3>
+                <p>Vous pouvez vous montrer en public!</p>
+              </div>
               <a href="{{ route('stream.index') }}"  class="btn  btn-lg gold">Regarder nos chaines </a>
               <a href="{{ route('register') }}"  class="btn  btn-lg gold">Créer votre chaine</a>
             </div>
           </div>
           <div class="carousel-item" style="background-image: url('img/casual-and-creative-at-home_4460x4460.jpg')">
             <div class="carousel-caption d-none d-md-block">
-              <h3>Quelque soit vos talents! Votre place est chez nous!</h3>
-              <p>Chaine, fun, amis...</p>
+                <div class="visible">
+                    <h3>Quelque soit vos talents! Votre place est chez nous!</h3>
+                    <p>Chaine, fun, amis...</p>
+                </div>
               <a href="{{ route('stream.index') }}"  class="btn  btn-lg gold">Regarder nos chaines </a>
               <a href="{{ route('register') }}"  class="btn  btn-lg gold">Créer votre chaine</a>
             </div>
