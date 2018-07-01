54ebbe532d8ab8d1b8ec1c51826a028de7688057
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-07-01 11:48:38 +0200

    Remplacement automatique de localhost lorsque le site en prod

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid top bottom">
 		<div class="row">
-			<iframe src="http://localhost:3001/?token={{$user->token}}" class="w-100 border-0"></iframe>
+			<iframe src="<?php echo str_replace(":8000","",Request::root()); ?>:3001/?token={{$user->token}}" class="w-100 border-0"></iframe>
 		</div>
     </div>
 @endsection
