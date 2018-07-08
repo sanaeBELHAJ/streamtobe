853e854d34b277c0f79315b3c1ec510080b23b29
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-07-08 22:41:22 +0200

    Correction du responsive design sur les /messages

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="container-fluid top bottom">
-			<div class="row">
-				<iframe src="<?php echo str_replace(":8000","",Request::root()); ?>:3001/?token={{$user->token}}" 
-						class="w-100 border-0"></iframe>
-			</div>
+		<div class="row">
+			<iframe src="<?php echo str_replace(":8000","",Request::root()); ?>:3001/?token={{$user->token}}" 
+					class="w-100 border-0"></iframe>
+		</div>
     </div>
 @endsection
 
 @section('css')
 <style>
 	iframe{
-		height: 59vh;
+		height: 65vh;
 	}
 </style>
 @endsection
