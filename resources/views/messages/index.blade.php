8f6b63ad840818fad354b80168885cbdb601b56f
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-07-13 11:25:46 +0200

    Changement de l'iframe pour les messages en prod

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -11,7 +11,7 @@
 			@if(env('APP_ENV') != "production")
 				<iframe src="http://localhost:3001/?token={{$user->token}}" class="w-100 h-100 border-0"></iframe>
 			@else
-				<iframe src="https://io.streamtobe.com/?token={{$user->token}}" class="w-100 h-100 border-0"></iframe>
+				<iframe src="https://mp.streamtobe.com/?token={{$user->token}}" class="w-100 h-100 border-0"></iframe>
 			@endif
 		</div>
 	</div>
