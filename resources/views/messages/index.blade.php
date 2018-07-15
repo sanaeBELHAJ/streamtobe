4b36d1fb7f46c582b490078ea88c63dd9c8421f3
sanae <sanai-son112@hotmail.fr>
2018-07-15 12:29:52 +0200

    chat style

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,13 +1,9 @@
 @extends('layouts.template')
 
 @section('content')
-	<div class="container-fluid top-2 bottom h-75">
+	<div class="container-fluid top-1 bottom h-75">
 		<div class="row h-100">
-			<p class="text-center mx-auto">
-					<small>Les utilisateurs à qui vous pouvez envoyer un message apparaissent dans la liste à gauche.</small>
-					<br>
-					<small><b>Vous devez vous abonner mutuellement à vos chaines respectives.</b></small>
-			</p>
+
 			@if(env('APP_ENV') != "production")
 				<iframe src="http://localhost:3001/?token={{$user->token}}" class="w-100 h-100 border-0"></iframe>
 			@else
