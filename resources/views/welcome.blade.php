4e64cad033f9bd5ad3dab252115ee2b21ec33b02
Yuu <youcef.messao@gmail.com>
2018-07-17 04:21:05 +0200

    page home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <div class="col-sm-12 pull-right bottom" style="margin-top: 50px;">
             <p>Bienvenue {{ Auth::user()->pseudo }},</p>
             <hr>
-            <p> Les chaines en live que vous suivez :</p>
+            <p>Les chaines en live que vous suivez :</p>
             
             <div class="row col-12">
                 <div class="">
@@ -19,39 +19,28 @@
                     @if(count($followed) > 0)
                         <div class="row text-center text-lg-left">
                             @foreach ($followed as $stream)
-                                <div class="col-12 col-sm-6 col-md-4" style="box-sizing: border-box;">
-                                    <a href="{{ route('stream.show', ['user' => $stream->user->pseudo]) }}" class="item">
-                                        <!--<img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">-->
-                                        <span class="watch">
-                                            <i class="material-icons gold-text" style="color:#f4eb19f0">settings_input_antenna</i>
-                                            {{ $stream->type->name }}
-                                        </span>
-                                        @if($stream->user->avatar!="users/defaullt.png")
-                                            <img class="img-fluid img-thumbnail" src="<?php echo asset('storage/'.$stream->user->avatar); ?>" alt="" title="Image de profil">
-                                        @else
-                                            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-                                        @endif
-                                    </a>
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <a class="broadcastname pull-right"  href="{{ route('stream.show', ['user' => $stream->user->pseudo]) }}" class="item">
-                                                {{ $stream->title }}
-                                            </a>
+                                <div class="col-6 col-sm-4 col-md-3">
+                                    <div class="card card-lg">
+                                        <div class="card-img">
+                                            <a href="/home/{{$stream->user->pseudo}}"><img  src="<?php echo asset('storage/' . $stream->user->avatar); ?>" class="card-img-top"></a>
+                                            @if ($viewer->user->stream->status == 1)
+                                                <div class="badge badge-xbox-one">En ligne</div>
+                                                <div class="badge badge-ps4" style="left:150px;">{{$stream->type->name}}</div>
+                                            @else
+                                                <div class="badge badge-steam">Hors ligne</div>
+                                            @endif
+                                            <div class="card-likes">
+                                                <a href="#"><img src="{{ $stream->user->country->svg }}" style="max-width: 200px;max-height: 30px;"></a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6">
-                                            <small>
-                                                <a class="broadcastname pull-right"  href="{{ route('stream.show', ['user' => $stream->user->pseudo]) }}" class="item">
-                                                    {{ $stream->user->pseudo }}
-                                                </a>
-                                            </small>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <img class="right" style="width:20%; padding-top: 4px" src="{{ $stream->user->country->svg }}">
+                                        <div class="card-block">
+                                            <h4 class="card-title"><a href="/home/{{$viewer->user->pseudo}}">{{$stream->user->pseudo}}</a></h4>
+                                            <div class="card-meta"><span>Inscrit le {{ Carbon\Carbon::parse($stream->user->created_at)->format('d/m/Y') }}</span></div>
+                                            <p class="card-text">{{$stream->title}}</p>
                                         </div>
                                     </div>
                                 </div>
+
                             @endforeach
                         </div>
                     @else
