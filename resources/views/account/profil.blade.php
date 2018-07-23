2226edd04bfdd97dbe6496ee5b943abf1bf07e5f
sanae <sanai-son112@hotmail.fr>
2018-07-23 11:22:13 +0200

    page profil v1

--- resources/views/account/profil.blade.php
+++ resources/views/account/profil.blade.php
@@ -1,83 +1,88 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="container bottom">
-    <h1 class="text-center">Profil de {{ $streamer->pseudo }}</h1>
-    <div class="row card card-lg">
-        <div class="card-block">
-            <div class="col-6">
-                <img class="pictureAccount" style="background-image:url(<?php echo asset('storage/'.$streamer->avatar); ?>)">
-            </div>
-            <div class="col-6 d-flex flex-row-reverse">
-                <p>Compte créé le: <?php echo date('d/m/Y', strtotime($streamer->created_at)); ?></p>
-            </div>
-            <h3>La chaine de streaming</h3>
-
-        <p class="col-12">Pays : <img src="{{ $streamer->country->svg }}" style="max-width: 200px;max-height: 30px;"></p>
-
-        <p class="col-12">{{ $streamer->pseudo }} est actuellement : <?php echo ($streamer->stream->status==0) ? "<span class='badge badge-steam'>Hors-ligne</span>" : "   <span class='badge badge-xbox-one'>En ligne</span>"; ?>.</p>
-        @if($streamer->stream->status==0 && $streamer->stream->updated_at)
-            <p class="col-12">Sa dernière diffusion remonte à <?php echo date('d/m/Y', strtotime($streamer->stream->updated_at)); ?></p>
-        @endif
-        
-        <p class="col-6 d-flex align-items-center">Vous pouvez accèder à sa chaine en cliquant directement sur la caméra : </p>
-        <p class="col-6 position-relative">
-            <a class="" href="{{ route('stream.show', ['user' => $streamer->pseudo]) }}">
-                <i class="fa fa-camera fa-5x">
-                </i>
-            </a>
-        </p>
-
-        @auth
-            <div class='offset-0 col-lg-2 col-sm-12 col-md-3 col-mb-2'>
-                @php ($IsCurrentViewer = 0)
+<div class="container-fluid profil">
+    <div class="row">
+<div class="col-lg-12 col-sm-12">
+    <div class="card hovercard">
+        <div class="card-background">
+            <img class="card-bkimg" alt="" src="<?php echo asset('storage/'.$streamer->avatar); ?>">
+            <!-- http://lorempixel.com/850/280/people/9/ -->
+        </div>
+        <div class="useravatar">
+            <img alt="" src="<?php echo asset('storage/'.$streamer->avatar); ?>">
+        </div>
+        <div class="card-info"> 
+            <span class="card-title">{{ $streamer->pseudo }}</span>
+        </div>
+    </div>
+    <div class="" role="group" aria-label="...">
+       <ul class="nav nav-tabs" role="tablist">
+           
+            <li class="nav-item">
+              <a class="nav-link active" href="#profile" role="tab" data-toggle="tab"><i class="fa fa-user"></i>A propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#machaine" role="tab" data-toggle="tab"><i class="fa fa-window-restore"></i>Ses chaînes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#follows" role="tab" data-toggle="tab"><i class="fa fa-heart"></i>Ses fans</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#revenus" role="tab" data-toggle="tab"><i class="fa fa-dollar"></i>Revenus </a>
+            </li>
+            <div class="col-sm-4"></div>
+            <div class="col-sm-2">
+              @php ($IsCurrentViewer = 0)
                 @foreach(Auth::user()->viewers as $viewer)
                     @if($streamer->stream->id == $viewer->stream_id)
-                        <button class="follow_stream w-100 float-none btn btn-follow @if($viewer->is_follower == 1) @else d-none @endif"
+                        <button class="col-sm-12 pull-right right follow_stream w-100 float-none btn btn-follow @if($viewer->is_follower == 1) @else d-none @endif"
                                 data-toggle="tooltip" data-placement="top" data-streamer="{{$streamer->pseudo}}"
                                 title="Retirer cette chaine de vos favoris" data-value="0" >Se désabonner <i class="fa fa-unlink"></i></button>
-                        <button class="follow_stream w-100 float-none btn btn-follow @if($viewer->is_follower == 0) @else d-none @endif"
+                        <button class="col-sm-12 pull-right right  follow_stream w-100 float-none btn btn-follow @if($viewer->is_follower == 0) @else d-none @endif"
                                 data-toggle="tooltip" data-placement="top" data-streamer="{{$streamer->pseudo}}"
                                 title="Mettre cette chaine dans vos favoris" data-value="1" >S'abonner <i class="fa fa-heart-o"></i></button>
                         @php ($IsCurrentViewer = 1)
                     @endif
-                </p>
-                <p>{{ $streamer->pseudo }} est actuellement : <?php echo ($streamer->stream->status == 0) ? "<span class='badge badge-steam'>Hors-ligne</span>" : "   <span class='badge badge-xbox-one'>En ligne</span>"; ?>.</p>
-                @if($streamer->stream->status==0 && $streamer->stream->updated_at)
-                <p>Sa dernière diffusion remonte à <?php echo date('d/m/Y', strtotime($streamer->stream->updated_at)); ?></p>
-                @endif
+                @endforeach
             </div>
-        @endauth
-    </div>
-    </div>
-    <div class="row card card-lg">
-        <div class="card-block">
-            <h3>Description de {{ $streamer->pseudo }}</h3>
+        </ul>
 
-            <p class="col-12">
-            @if($streamer->description)
-                {{ $streamer->description }} 
-                @else 
-                Cet utilisateur n'a pas encore rédigé de description
-            @endif
-        </p>
-    </div>
-    </div>
-        <div class="row card card-lg">
-            <div class="card-block">
-        <h3>Statistiques</h3>
-        <p class="col-12">Vous pouvez consulter des informations complémentaires à propos de la chaine de {{ $streamer->pseudo }} :</p>
-            <a class="btn btn-facebook btn-lg btn-rounded m-l-10" href="/follows/{{$streamer->pseudo}}">Ses followers  <i class="fa fa-user"></i></a>
-            <a class="btn btn-danger btn-lg btn-rounded m-l-10" href="/fans/{{$streamer->pseudo}}">Ses chaînes <i class="fa fa-heart"></i></a>
-            @if(Auth::check() && $streamer->pseudo == Auth::user()->pseudo)
-            <a class="btn btn-primary btn-lg btn-rounded m-l-10" href="/stats/{{$streamer->pseudo}}">Dons <i class="fa fa-dollar"></i></a>
-            @endif
-        </div>-->
-    </div>
+<!-- Tab panes --> 
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane fade show active" id="profile">
+        <div class="row">
+            <div class="col-sm-12">
+                <p class="col-12">Membre depuis le <?php echo date('d/m/Y', strtotime($streamer->created_at)); ?></p>
+                <p class="col-12">Pays : <img src="{{ $streamer->country->svg }}" style="max-width: 200px;max-height: 30px;"></p>
+
+                <p class="col-12">{{ $streamer->pseudo }} est actuellement : <?php echo ($streamer->stream->status==0) ? "<span class='badge badge-steam'>Hors-ligne</span>" : "   <span class='badge badge-xbox-one'>En ligne</span>"; ?>.</p>
+                @if($streamer->stream->status==0 && $streamer->stream->updated_at)
+                    <p class="col-12">Sa dernière diffusion remonte à <?php echo date('d/m/Y', strtotime($streamer->stream->updated_at)); ?></p>
+                @endif
+
+                <p class="col-6 d-flex align-items-center">Vous pouvez accèder à sa chaine en cliquant directement sur la caméra : </p>
+                <p class="col-6 position-relative">
+                    <a class="" href="{{ route('stream.show', ['user' => $streamer->pseudo]) }}">
+                        <i class="fa fa-camera fa-5x">
+                        </i>
+                    </a>
+                </p>
+            </div>
         </div>
+    </div>
+  <div role="tabpanel" class="tab-pane fade" id="machaine">bbb</div>
+  <div role="tabpanel" class="tab-pane fade" id="follows">ccc</div>
+  <div role="tabpanel" class="tab-pane fade" id="revenus">ccc</div>
 </div>
-
+ 
+    </div>
+    </div>
+    
+</div>  
+</div>
 @endsection
+
 
     @section('css')
     <style>
