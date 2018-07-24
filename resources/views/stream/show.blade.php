b13f2c1354ff39765eb4063bda81f4880bea8633
Yuu <youcef.messao@gmail.com>
2018-07-24 04:13:36 +0200

    filtre

--- resources/views/stream/show.blade.php
+++ resources/views/stream/show.blade.php
@@ -193,7 +193,7 @@
                             {{-- Giveaway --}}
                             <p class="col-lg-4 col-sm-12 col-md-4 col-mb-12 text-center">
                                 <input type="hidden" name="streamer_name" value="{{ $streamer->pseudo }}">
-                                <button class="btn btn-primary btn-lg btn-rounded m-l-10" style="margin-top: 0px;padding: 16px 27px;color:white!important;" data-toggle="modal"
+                                <button class="btn btn-primary btn-lg btn-rounded m-l-10" style="margin-top: 0px;margin-bottom: 30px;padding: 16px 27px;color:white!important;" data-toggle="modal"
                                     data-target="#paymentModal">Faire un don <i class="fa fa-dollar"></i></button>
                             </p>
                             @include('stream.modal.payment')
